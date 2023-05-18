f7a696f9fdac539d8f7b29b1ec2dbee9d8dc1919
Eduardo Heyer <eduardo.heyer12@gmail.com>
2023-05-18 15:55:12 -0400

    started react app for mrsmooth

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        Mr.Smooth
+        <span className="logo" role="img"> 🫗 </span>
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
